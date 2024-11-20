50eee7a58b024ab81cd1241f45568aa0e9a75f61
lkb7086 <35011865+lkb7086@users.noreply.github.com>
2024-11-20 15:48:20 +0900

    Update IAuthService.cs

--- Services/Interfaces/IAuthService.cs
+++ Services/Interfaces/IAuthService.cs
@@ -1,8 +1,11 @@
-﻿using Sample.Repository.Interfaces;
+using Sample.Common;
+using Sample.Models.DTO;
+using Sample.Repository.Interfaces;
 
 namespace Sample.Services.Interfaces
 {
     public interface IAuthService
-    {      
+    {
+        public Task<LoginRes> AuthLogin(ConnectionManager conMgr, NLog.Logger log, SemaphoreSlim semaphore, LoginReq req);
     }
 }
